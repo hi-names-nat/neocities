c014606b7fa1f45c23da7667c7e79257953b9043
Natalie <natalie.soltis@outlook.com>
2025-05-28 21:44:01 -0400

    add files for pages

---
+++ creation/index.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>HEX/HOME</title>
+    <link href="style.css" rel="stylesheet" type="text/css" media="all">
+</head>
+<body>
+<div id="content" class='content'>
+    <h1 class="h0" aria-hidden="true" role="presentation">creationcreationcreationcreation<b class="h0_inner" aria-hidden="false">Creation</b>creationcreationcreationcreation</h1>
+    <blockquote>Awah</blockquote>
+</div>
+<?php require '../_php_components/navbar.php' ?>
+</body>
+</html>
