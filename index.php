0044525836d58e25cde25c1422e26ba8da05a705
Natalie <natalie.soltis@outlook.com>
2025-06-30 19:37:43 -0400

    Disclaimer restyle

--- index.php
+++ index.php
@@ -10,21 +10,48 @@
 <body class='bg_cyan'>
 <div>
     <div class="center_div">
-        <div class="dos_install_popup center_box font_ibm">
-            <h3 class="dos_install_header">Warning</h3>
-            <p>This website uses a bit of minimal Javascript to do some nicety visual and page state things. Otherwise
-            it is entirely viewable JSless.</p>
-            <p>While this page should work with all web browsers, I used Firefox while designing it and would recommend
-            Firefox (or a fork). This page is designed for desktops, and viewing it on mobile may be awkward.</p>
-            <p>All content on this site is hosted entirely within this site. The site will never contain embeds, libraries, tracking
-            or such hosted on other servers.</p>
-            <p>All content hosted on this site (and on my other platforms) utilized no "Artificial Intelligence" generative algorithms.
-            This shit's made by my own claws, babe.</p>
-            <b>I do not consent for any of the content here or anywhere else to be used as training data for any sort of
-                bot, AI model or other sort of AI algorithm.</b>
+        <div class="dos_install_popup center_box font_ibm dos-31-style">
+            <h3 class="win31_tablebar">NOTICE</h3>
+            <table>
+                <tr>
+                    <td style="background-color: transparent">
+                        <img src="/_assets/win_q0.PNG" alt="A question mark, with a drop-shadow."
+                    </td>
+                    <td style="background-color: transparent">
+                        <p>This website uses a bit of minimal Javascript to do some nicety visual and page state things. Otherwise
+                        it is entirely viewable JSless.</p>
+                        <p>While this page should work with all web browsers, I used Firefox while designing it and would recommend
+                        Firefox (or a fork). This page is designed for desktops, and viewing it on mobile may be awkward.</p>
+                        <p>All content on this site is hosted entirely within this site. The site will never contain embeds, libraries, tracking
+                        or such hosted on other servers.</p>
+                        <p>All content hosted on this site (and on my other platforms) utilized no "Artificial Intelligence" generative algorithms.
+                        This shit's made by my own claws, babe.</p>
+                        <b>I do not consent for any of the content here or anywhere else to be used as training data for any sort of
+                            bot, AI model or other sort of AI algorithm.</b>
+                    </td>
+                </tr>
+            </table>
             <div style="display: flex; justify-content: center"><a href="home.php"><button class="dos-button">OK</button></a></div>
         </div>
     </div>
+    <table class="dos-31-desktop-table" aria-hidden="true">
+        <tr>
+            <td align="center">
+                <img src="_assets/win_files.PNG">
+                <p class="dos_31_text">Files</p>
+            </td>
+            <td align="center">
+                <img src="_assets/win_term.PNG">
+                <p class="dos_31_text">Terminal</p>
+            </td>
+        </tr>
+        <tr>
+            <td style="background-color: transparent">
+                <img src="_assets/win_control.PNG">
+                <p class="dos_31_text">Control Panel</p>
+            </td>
+        </tr>
+    </table>
 </div>
 </body>
 </html>
